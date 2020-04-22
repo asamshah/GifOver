059769913820ee977eb8f37912c109dcf53edca6
Asam Shah <asamshah@gmail.com>
2020-04-22 17:37:47 +0100

    Updates

---
+++ precache-manifest.f63da3c6e666fc2f5b6e600ed210aa75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855ad579dc596538abe2af6c8826a63c",
+    "url": "/gifover/index.html"
+  },
+  {
+    "revision": "77403bba8f5a52da47cb",
+    "url": "/gifover/static/css/main.5b5e225f.chunk.css"
+  },
+  {
+    "revision": "743bb07d523f3636debd",
+    "url": "/gifover/static/js/2.3298eb6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gifover/static/js/2.3298eb6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77403bba8f5a52da47cb",
+    "url": "/gifover/static/js/main.0ed58606.chunk.js"
+  },
+  {
+    "revision": "c7ca2e1eb0eba6519a9c",
+    "url": "/gifover/static/js/runtime-main.d745c87b.js"
+  },
+  {
+    "revision": "5b9a3ae1255e28dc936234fe3c03e06c",
+    "url": "/gifover/static/media/close-icon.5b9a3ae1.svg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/gifover/static/media/loader.be00fc4a.svg"
+  }
+]);
